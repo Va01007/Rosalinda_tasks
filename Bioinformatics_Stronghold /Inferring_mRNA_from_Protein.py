a132bac58c5f43709a2566ab3238e52b33b8410c
Va01007 <108982082+Va01007@users.noreply.github.com>
2025-07-07 17:45:07 +0300

    added a commentary

--- Bioinformatics_Stronghold /Inferring_mRNA_from_Protein.py
+++ Bioinformatics_Stronghold /Inferring_mRNA_from_Protein.py
@@ -13,3 +13,5 @@ def Infer_mRNA(in_file:str):
 if __name__ == "__main__":
     prot_dict = {"F":2, "L":6, "S":6, "Y":2, "!":3, "W":1, "P":4, "H":2, "Q":2, "R":6, "I":3, "M":1, "V":4, "A":4, "D":2, "E":2, "G":4, "K":2, "T":4, "N":2, "C":2}
     print(Infer_mRNA(sys.argv[1]))
+
+#yeah, it was without modular arithmetic. But if i can do this how will stop me?
